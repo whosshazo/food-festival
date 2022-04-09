8a1db8a2d671aac7ee4732976f2e172ffb47fcfc
whosshazo <ryandeshazo1@gmail.com>
2022-04-08 21:32:20 -0400

    service worker activation

--- assets/js/domMethods.js
+++ assets/js/domMethods.js
@@ -1,76 +1,37 @@
-require("bootstrap");
-
-$(document).ready(function () {
-  function createEl(htmlString, attrs, ...children) {
-    if (typeof htmlString !== "string") {
-      throw Error("Argument 'htmlString' is required and must be a string");
-    }
-
-    const el = document.createElement(htmlString);
-
-    if (typeof attrs === "object") {
-      for (let key in attrs) {
-        if (key.substring(0, 2) === "on") {
-          el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-        } else {
-          el.setAttribute(key, attrs[key]);
-        }
-      }
-    }
-
-    children.forEach(function (child) {
-      let node;
-
-      if (child.constructor.name.includes("Element")) {
-        node = child;
-      } else {
-        node = document.createTextNode(child);
-      }
-
-      el.appendChild(node);
-    });
-
-    return el;
+function createEl(htmlString, attrs, ...children) {
+  if (typeof htmlString !== "string") {
+    throw Error("Argument 'htmlString' is required and must be a string");
   }
 
-  // First image is hard coded in index.html
-  const carouselSlides = [
-    {
-      title: "We travel all over the US",
-      subtitle: "Check out our schedule!",
-      img: img1,
-      btnText: "View Schedule",
-      btnUrl: "schedule.html",
-    },
-    {
-      title: "Our food is seriously the bomb!",
-      subtitle: "What are you waiting for?",
-      img: img2,
-      btnText: "Purchase Tickets",
-      btnUrl: "tickets.html",
-    },
-  ];
+  const el = document.createElement(htmlString);
 
-  carouselSlides.forEach((slide, i) => {
-    $(".carousel-inner").append(`
-  <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
-    <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-              <h2 class="display-4 mb-2">${slide.title}</h2>
-          </div>
-          <div class="row align-items-center justify-content-center"> 
-            <h3>${slide.subtitle}</h3>
-          </div>
-          <div class=" mt-4 row align-items-center justify-content-center"> 
-            <a class="btn btn-primary" href="${slide.btnUrl}">
-                ${slide.btnText}
-            </a>
-          </div>
-        </div>
-    </div>
-  </div>`);
+  if (typeof attrs === "object") {
+    for (let key in attrs) {
+      if (key.substring(0, 2) === "on") {
+        el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+      } else if (key === "style") {
+        for (let rule in attrs[key]) {
+          el.style[rule] = attrs[key][rule];
+        }
+      } else {
+        el.setAttribute(key, attrs[key]);
+      }
+    }
+  }
+
+  children.forEach(function (child) {
+    let node;
+
+    if (child.constructor.name.includes("Element")) {
+      node = child;
+    } else {
+      node = document.createTextNode(child);
+    }
+
+    el.appendChild(node);
   });
-});
+
+  return el;
+}
 
 module.exports = createEl;
--- assets/js/events.js
+++ assets/js/events.js
@@ -1,88 +1,43 @@
 require("bootstrap");
 const createEl = require("./domMethods");
-const createLoremIpsum = require("./helpers");
 
 if (window.location.href.indexOf("event") > -1) {
-  function onEventClick(calEvent) {
-    const start = dateConverter(calEvent.start);
-    localStorage.setItem(
-      "currentEvent",
-      JSON.stringify({
-        title: calEvent.title,
-        subtitle: start,
-        description: calEvent.description,
-        image: calEvent.image,
-      })
-    );
+  const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+    title: "Title Placeholder",
+    subtitle: "",
+    description: "",
+  };
 
-    window.location.href = "events.html";
-  }
+  const pageEl = document.querySelector("#page");
 
-  const events = [
-    {
-      title: "Vegan Day",
-      start: new Date(y, m + 1, 20),
-      description: `Vegans unite! At this event, we'll be celebrating with vegan food from around the globe. With so many delicious vegan cuisine options, it's hard to pick one. What are you hungry for? Let us share with you!
-          Veganism is a lifestyle choice—we're passionate about creating healthy and delicious food products without harming the environment, so please spread the word about our festival and about our work!`,
-      image:
-        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    },
-    {
-      title: "Harvest Festival",
-      start: new Date(y, m + 1, 20),
-      image:
-        "https://images.unsplash.com/photo-1474440692490-2e83ae13ba29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    },
-    {
-      title: "Bacon Day",
-      start: new Date(y, m + 1, 20),
-      description: `Today's all about bacon! We'll be serving up the best tasting bacon around town! Let's all get excited about bacon together!
-          As an appetizer, grab a side of our delicious BBQ pulled pork and pick up a delicious side of our homemade slaw and sauce! There will be bacon everywhere!!`,
-      image:
-        "https://images.unsplash.com/photo-1528607929212-2636ec44253e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
-    },
-    {
-      title: "Fry-day",
-      description: `You bring it, we fry it. Everything tastes better fried! Whether it's your favorite sponge cake or a refreshing afternoon snack,
-          prepare to shock your tastebuds.`,
-      start: new Date(y, m + 1, 21),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-    {
-      title: "Pizza Party!",
-      description: `This event is devoted to all those pizza lovers out there. Come enjoy fresh from the oven pizza and learn tips on making pizza at home from the pros!`,
-      start: new Date(y, m + 1, 22),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-    {
-      title: "Pasta Bonanza",
-      description: `All things pasta.`,
-      start: new Date(y, m + 1, 22),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-    {
-      title: "Fruity Foods!",
-      description: `We will be showcasing dishes where fruits are a primary ingredient. Come learn how you can add more fruits into your diet!`,
-      start: new Date(y, m + 1, 22),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-    {
-      title: "Veggie Mix Up",
-      description: `We will be showcasing great dishes that have plenty of veggies and proteins! This is for all those out there who love food, but want to get more vegetables in their diets!`,
-      start: new Date(y, m + 1, 23),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-    {
-      title: "Fry-day",
-      description: `You bring it, we fry it....again. This event is always a huge hit, so we hold it twice!!`,
-      start: new Date(y, m + 1, 23),
-      image:
-        "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
-    },
-  ];
+  const containerEl = createEl(
+    "div",
+    { class: "container" },
+    createEl(
+      "div",
+      { class: "card mb-3" },
+      createEl("img", {
+        class: "card-img-top",
+        style: "width: 5px",
+        src: currentEvent.image || "https://via.placeholder.com/350x150",
+      }),
+      createEl(
+        "div",
+        { class: "card-body" },
+        createEl("h1", { class: "card-title" }, currentEvent.title || ""),
+        createEl("h2", { class: "text-muted" }, currentEvent.subtitle || ""),
+        createEl(
+          "p",
+          { class: "card-text mt-3" },
+          currentEvent.description || createLoremIpsum(100)
+        ),
+        createEl(
+          "a",
+          { class: "btn btn-primary", href: "tickets.html" },
+          "Buy Tickets"
+        )
+      )
+    )
+  );
+  pageEl.appendChild(containerEl);
 }
